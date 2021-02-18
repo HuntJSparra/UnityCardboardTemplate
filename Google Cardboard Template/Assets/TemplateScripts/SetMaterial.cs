646d3de1a14eaccc645cdf3297df467dbf0ebfe6
Hunt Sparra <hjs@Hunts-iMac.local>
2021-02-18 02:30:35 -0500

    Added Template Scene with iOS and Android build suppport

---
+++ Google Cardboard Template/Assets/TemplateScripts/SetMaterial.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This class is part of an example of how you can use UnityEvents with
+// basic MonoBehaviors to rapidly develop VR interactions.
+//
+// See: GazeOverEvent.cs
+public class SetMaterial : MonoBehaviour
+{
+    public void UpdateMaterial(Material newMat)
+    {
+        GetComponent<MeshRenderer>().material = newMat;
+    }
+}
